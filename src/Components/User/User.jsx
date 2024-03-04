1da891e367d267c44a9b7bf0a4719e2e0243e9b4
amoghga57 <amoghga0@gmail.com>
2024-03-05 02:03:46 +0530

    changed the path

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import LogoCommon from '../Common components/LogoCommon'
 import "./user.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -10,23 +10,35 @@ const User = () => {
   let data =
     [
       { "id": 1, "name": "Arjun", "dob": "16-11-2000", "role": "Software Engineer" },
-      { "id": 2, "name": "Mahesh", "dob": "16-11-2000", "role": "Software Engineer" },
-      { "id": 3, "name": "Suresh", "dob": "16-11-2000", "role": "Software Engineer" },
-      { "id": 4, "name": "Ramesh", "dob": "16-11-2000", "role": "Software Engineer" },
-      { "id": 5, "name": "Surjan", "dob": "16-11-2000", "role": "Software Engineer" },
-      { "id": 6, "name": "Darshan", "dob": "16-11-2000", "role": "Software Engineer" },
-      { "id": 7, "name": "Robert", "dob": "16-11-2000", "role": "Software Engineer" },
-      { "id": 8, "name": "Chad", "dob": "16-11-2000", "role": "Software Engineer" },
-      { "id": 9, "name": "Suraj", "dob": "16-11-2000", "role": "Software Engineer" },
-      { "id": 10, "name": "Arjun", "dob": "16-11-2000", "role": "Software Engineer" },
-
+      { "id": 2, "name": "Mahesh", "dob": "05-05-2022", "role": "Web Developer" },
+      { "id": 3, "name": "Suresh", "dob": "11-07-2000", "role": "Backend developer" },
+      { "id": 4, "name": "Ramesh", "dob": "07-10-2000", "role": "Software Engineer" },
+      { "id": 5, "name": "Surjan", "dob": "21-01-2008", "role": "Cloud Engineer" },
+      { "id": 6, "name": "Darshan", "dob": "31-11-2000", "role": "Architecture Engineer" },
+      { "id": 7, "name": "Robert", "dob": "20-06-2000", "role": "Frontend Developer" },
+      { "id": 8, "name": "Chad", "dob": "15-12-2000", "role": "DevOps" },
+      { "id": 9, "name": "Suraj", "dob": "25-02-2000", "role": "Technical Support" },
+      { "id": 10, "name": "Arjun", "dob": "01-05-2000", "role": "AI Engineer" },
     ]
 
   const [Names, setNames] = useState("")
 
+
   const filteredData = data.filter(item => item.name.toLowerCase().includes(Names.toLowerCase()));
+
+  useEffect(() => {
+    if (filteredData.length <= 0) {
+      document.getElementById("notFound").style.display = "flow-root"
+    } else {
+      // document.getElementById("notFound").style.visibility="hidden"
+      document.getElementById("notFound").style.display = "none"
+    }
+  }, [filteredData])
+
+
   return (
     <div id='main'>
+
       <div id="fixed_boxs">
         <LogoCommon />
         <div>
@@ -36,8 +48,11 @@ const User = () => {
           </div>
         </div>
       </div>
-
+      <label id='notFound' >
+        <div>NO PERSON FOUND</div>
+      </label>
       <div id="info">
+
         {filteredData.map((x) => {
           return (
             <div id="boxs">
@@ -49,10 +64,13 @@ const User = () => {
           )
         })}
       </div>
+
       <div>
         <footer></footer>
       </div>
-      <Navigation/>
+
+      <Navigation />
+
     </div>
   )
 }
